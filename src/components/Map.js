941bdba096ec3fe8cb41f2812c7c17f86408a15c
Didac Bigorda <didacbigorda@gmail.com>
2019-03-01 10:27:18 +0100

    Change to map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,20 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import MapboxGl from 'mapbox-gl/dist/mapbox-gl.js';
 import '../styles/map.css';
 
-<<<<<<< HEAD
-//mapboxgl.accessToken =;
-
-export default class Map extends Component {
-=======
 class Map extends Component {
->>>>>>> 281dd28e019181898898feb8f18531e0c2f17a8c
 
   componentDidMount() {
-    MapboxGl.accessToken = 'pk.eyJ1IjoiYWplciIsImEiOiJjanNvd3N3NzEwYWN3NDNtenJuZzF2NzQxIn0.IdZ59iukLwHQ_Ak0yK8Ymw'
+    MapboxGl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
     new MapboxGl.Map({
       container: this.container,
-      style: 'mapbox://styles/ajer/cjsp3c4s03rzb1gk4ocy4zckt',
+      style: 'mapbox://styles/mapbox/light-v9',
       center: [2.188297, 41.388424],
       zoom: 16.6,
     })
