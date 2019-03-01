de0ccd1629602bbbde57dec17a2b5ba735295614
Miguelerja <miguelerja@gmail.com>
2019-03-01 13:09:06 +0100

    Add marker and popup

--- src/components/Map.js
+++ src/components/Map.js
@@ -12,28 +12,50 @@ export default class Map extends Component {
     trackUserLocation: true
 });
 
+  listBooks = () => {
+    const books = this.props;
+
+    books.map(book => {
+      const popup = new mapboxgl.Popup()
+        .setHTML(
+          `<button>book.info.author</button>`
+          );
+      return
+    });
+  }
+
   componentDidMount(){
     const { token } = this.props;
+    const popup = new mapboxgl.Popup()
+      .setHTML('<button>author</button>');
     const mapConfig = {
       container: 'map',
       style: 'mapbox://styles/ajer/cjsp3c4s03rzb1gk4ocy4zckt',
       center: [41.39, 2.15], 
       zoom: 9,
     };
-
     mapboxgl.accessToken = token;
+    
+    
     this.map = new mapboxgl.Map(mapConfig);
-
+    
     this.map.on('load', () => {
       // Add geolocate control to the map.
       this.map.addControl(this.geolocate);
       this.map.addControl(new mapboxgl.NavigationControl());
     });
+
+    this.marker = new mapboxgl.Marker()
+      .setLngLat([41.39, 2.15])
+      .addTo(this.map)
+      .setPopup(popup)
+      .togglePopup();
   }
 
   render(){
     return (
-      <div class='map' id='map'></div>
+      <div class='map' id='map'>
+      </div>
     );
   }
 }
