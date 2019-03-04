caf774eb4dd124e5b512ce9a9aee00d9b699e41b
Didac Bigorda <didacbigorda@gmail.com>
2019-03-04 23:04:37 +0100

    I think it is working?

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react'
 
 export default class TestComponent extends Component {
 
-  componentDidMount() {
+  state = {
+    books: this.props.books,
+  }
+
+  componentWillMount() {
     const idToFind = this.props.node.firstChild.getAttribute('id');
     const {books} = this.props;
-    this.rightBook = books.find((book) => {
-      return book.id == idToFind;
+    const rightBook = books.find((book) => {
+      return book._id === idToFind;
     })
+    this.setState({
+      book: rightBook,
+    }, () => console.log(this.state))
   }
 
   render() {
-    console.log(this.props.node.firstChild.getAttribute('id'))
+    //console.log(this.props.node.firstChild.getAttribute('id'))
     console.log(this.props)
-
+    const {book} = this.state;
+    console.log(book);
     return (
       <h1>
-        {this.rightBook.title}
+        {(this.state.book !== undefined) ? this.state.book.info.title : null }
       </h1>
     )
   }
